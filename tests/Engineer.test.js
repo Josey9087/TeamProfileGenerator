9a8b2be903a2ad062ad853a7f4999f9d89699ce4
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-11-03 14:34:24 -0600

    Intern test and js file

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -3,53 +3,53 @@ const Engineer = require("../lib/Engineer");
 
 describe('Employee', () => {
     describe("Constructor", () => {
-        it("Creates a new Employee", () => {
+        it("Creates a new Engineer", () => {
             const Engineertest = new Engineer();
             expect(typeof Engineertest).toBe("object");
         });
 
         describe("Name", () => {
-            it('Should return the employee Name', () => {
+            it('Should return the Engineer Name', () => {
                 expect(new Engineer('Jose').name).toEqual('Jose')
             })
         });
 
         describe("ID", () => {
-            it('Should return the employee ID', () => {
+            it('Should return the Engineer ID', () => {
                 expect(new Engineer('Jose', 5).id).toEqual(5)
             })
         });
 
         describe("Email", () => {
-            it('Should return the employee Email', () => {
+            it('Should return the Engineer Email', () => {
                 expect(new Engineer('Jose', 5, 'JY@gmail.com').email).toEqual('JY@gmail.com')
             })
         });
     })
     describe("getName", () => {
-        it('Should return the employee Name', () => {
+        it('Should return the Engineer Name', () => {
             expect(new Engineer('Jose', 5, 'JY@gmail.com').getName()).toEqual('Jose')
         })
     });
 
     describe("getID", () => {
-        it('Should return the employee ID', () => {
+        it('Should return the Engineer ID', () => {
             expect(new Engineer('Jose', 5, 'JY@gmail.com').getID()).toEqual(5)
         })
     });
 
     describe("getEmail", () => {
-        it('Should return the employee Email', () => {
+        it('Should return the Engineer Email', () => {
             expect(new Engineer('Jose', 5, 'JY@gmail.com').getEmail()).toEqual('JY@gmail.com')
         })
     });
     describe("getRole", () => {
-        it('Should return the engineer role', () => {
+        it('Should return the Engineer role', () => {
             expect(new Engineer('Jose', 5, 'JY@gmail.com').getRole()).toEqual('Engineer')
         })
     });
     describe("getGithub", () => {
-        it('Should return the github username', () => {
+        it('Should return the Engineer github username', () => {
             expect(new Engineer('Jose', 5, 'JY@gmail.com', 'Josey9087').getGithub()).toEqual('Josey9087')
         })
     });
---
+++ tests/Intern.test.js
@@ -0,0 +1,56 @@
+const { describe, it, expect } = require("@jest/globals");
+const Intern = require("../lib/Intern"); 
+
+describe('Employee', () => {
+    describe("Constructor ", () => {
+        it("Creates a new Intern", () => {
+            const Interntest = new Intern();
+            expect(typeof Interntest).toBe("object");
+        });
+
+        describe("Name", () => {
+            it('Should return the Intern Name', () => {
+                expect(new Intern('Jose').name).toEqual('Jose')
+            })
+        });
+
+        describe("ID", () => {
+            it('Should return the Intern ID', () => {
+                expect(new Intern('Jose', 5).id).toEqual(5)
+            })
+        });
+
+        describe("Email", () => {
+            it('Should return the Intern Email', () => {
+                expect(new Intern('Jose', 5, 'JY@gmail.com').email).toEqual('JY@gmail.com')
+            })
+        });
+    })
+    describe("getName", () => {
+        it('Should return the Intern Name', () => {
+            expect(new Intern('Jose', 5, 'JY@gmail.com').getName()).toEqual('Jose')
+        })
+    });
+
+    describe("getID", () => {
+        it('Should return the Intern ID', () => {
+            expect(new Intern('Jose', 5, 'JY@gmail.com').getID()).toEqual(5)
+        })
+    });
+
+    describe("getEmail", () => {
+        it('Should return the Intern Email', () => {
+            expect(new Intern('Jose', 5, 'JY@gmail.com').getEmail()).toEqual('JY@gmail.com')
+        })
+    });
+    describe("getRole", () => {
+        it('Should return the Intern role', () => {
+            expect(new Intern('Jose', 5, 'JY@gmail.com').getRole()).toEqual('Intern')
+        })
+    });
+    describe("getSchool", ()=>{
+        it('Should return the Intern school', () =>{
+            expect(new Intern('Jose', 5, 'JY@gmail.com', 'DU').getSchool()).toEqual('DU')
+        })
+    })
+});
