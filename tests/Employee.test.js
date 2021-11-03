18443b00d1f8deba853eff6aea5d7f1e4c85bfd7
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-11-03 13:54:29 -0600

    Adding employee test file and js file

---
+++ tests/Employee.test.js
@@ -0,0 +1,52 @@
+const { describe, it, expect } = require("@jest/globals");
+const Employee = require("../lib/Employee"); 
+
+describe('Employee', () => {
+    describe("Constructor ", () => {
+        it("Creates a new Employee", () => {
+            const Employeetest = new Employee();
+            expect(typeof Employeetest).toBe("object");
+        });
+
+        describe("Name", () => {
+            it('Should return the employee Name', () => {
+                expect(new Employee('Jose').name).toEqual('Jose')
+            })
+        });
+
+        describe("ID", () => {
+            it('Should return the employee ID', () => {
+                expect(new Employee('Jose', 5).id).toEqual(5)
+            })
+        });
+
+        describe("Email", () => {
+            it('Should return the employee Email', () => {
+                expect(new Employee('Jose', 5, 'JY@gmail.com').email).toEqual('JY@gmail.com')
+            })
+        });
+    })
+    describe("getName", () => {
+        it('Should return the employee Name', () => {
+            expect(new Employee('Jose', 5, 'JY@gmail.com').getName()).toEqual('Jose')
+        })
+    });
+
+    describe("getID", () => {
+        it('Should return the employee ID', () => {
+            expect(new Employee('Jose', 5, 'JY@gmail.com').getID()).toEqual(5)
+        })
+    });
+
+    describe("getEmail", () => {
+        it('Should return the employee Email', () => {
+            expect(new Employee('Jose', 5, 'JY@gmail.com').getEmail()).toEqual('JY@gmail.com')
+        })
+    });
+    describe("getRole", () => {
+        it('Should return the employee role', () => {
+            expect(new Employee('Jose', 5, 'JY@gmail.com').getRole()).toEqual('Employee')
+        })
+    });
+
+});
